8815c90c8faf3d5c34ada216b9be44a9b324c81b
Your GitHub Usernamemsdubova <femme777@gmail.com>
2024-06-30 13:55:51 +0100

    Added users input

--- cmd/password-manager.go
+++ cmd/password-manager.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("Вітаю у менеджері паролів. Оберіть опцію да введіть її , натисніть enter щоб продоавжити")
+
+	var userChoice int
+
+	fmt.Scan(&userChoice)
+
+	fmt.Printf("User's choice -  %v", userChoice)
 }
